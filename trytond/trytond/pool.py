1dc5f4f62882734d357847a95b395f28dad5ab4b
Jean Cavallo <jean.cavallo@hotmail.fr>
2026-02-07 23:02:20 +0100

    PATCH C2: ServerContext

--- trytond/trytond/pool.py
+++ trytond/trytond/pool.py
@@ -7,6 +7,7 @@ from threading import RLock, local
 from weakref import WeakSet
 
 from trytond.modules import load_modules, register_classes
+from trytond.server_context import ServerContext
 from trytond.transaction import Transaction
 
 __all__ = ['Pool', 'PoolMeta', 'PoolBase', 'isregisteredby']
@@ -140,9 +141,10 @@ class Pool(object):
             # Clear before loading modules
             self._pool = defaultdict(dict)
             self._modules = []
-            restart = not load_modules(
-                self.database_name, self, update=update, lang=lang,
-                activatedeps=activatedeps, indexes=indexes)
+            with ServerContext().set_context(disable_auto_cache=True):
+                restart = not load_modules(
+                    self.database_name, self, update=update, lang=lang,
+                    activatedeps=activatedeps, indexes=indexes)
             self._pools[self.database_name] = self._pool
             self._pool_modules[self.database_name] = self._modules
             self._pool_instances.clear()
