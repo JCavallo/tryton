ed53181fcf91df2481c5ce6ac57922eb7e062811
Jean Cavallo <jean.cavallo@hotmail.fr>
2026-02-08 17:05:38 +0100

    PATCH C9: Gunicorn/uWSGI production support

--- trytond/trytond/application.py
+++ trytond/trytond/application.py
@@ -1,23 +1,33 @@
 # This file is part of Tryton.  The COPYRIGHT file at the top level of
 # this repository contains the full copyright notices and license terms.
 import csv
+import logging
 import logging.config
 import os
 import threading
+import datetime
+# uwsgdecorators are not available when using gunicorn
+try:
+    import uwsgidecorators
+except ModuleNotFoundError:
+    uwsgidecorators = None
 from io import StringIO
 
-__all__ = ['app']
+__all__ = ['app', 'application']
+
+log_file = os.environ.get('WSGI_LOG_FILE')
+log_level = os.environ.get('LOG_LEVEL', 'ERROR')
+if log_file:
+    logging.basicConfig(level=getattr(logging, log_level),
+        filename=log_file)
 
 # Logging must be set before importing
 if logging_config := os.environ.get('TRYTOND_LOGGING_CONFIG'):
     logging.config.fileConfig(logging_config)
 else:
-    logging_level = int(
-        os.environ.get('TRYTOND_LOGGING_LEVEL') or logging.ERROR)
-    logformat = ('%(process)s %(thread)s [%(asctime)s] '
-        '%(levelname)s %(name)s %(message)s')
-    level = max(logging_level, logging.NOTSET)
-    logging.basicConfig(level=level, format=logformat)
+    LF = ('%(process)s %(thread)s [%(asctime)s] %(levelname)s'
+        ' %(name)s %(message)s')
+    logging.basicConfig(level=getattr(logging, log_level), format=LF)
 logging.captureWarnings(True)
 
 if os.environ.get('TRYTOND_COROUTINE'):
@@ -31,11 +41,13 @@ Pool.start_app_initialization()
 Pool.start()
 # TRYTOND_CONFIG it's managed by importing config
 db_names = os.environ.get('TRYTOND_DATABASE_NAMES')
+db_list = []
 if db_names:
     # Read with csv so database name can include special chars
     reader = csv.reader(StringIO(db_names))
     threads = []
     for name in next(reader):
+        db_list.append(name)
         thread = threading.Thread(target=lambda: Pool(name).init())
         thread.start()
         threads.append(thread)
@@ -65,3 +77,30 @@ application = app
 
 Pool.app_initialization_completed()
 assert len(threads := threading.enumerate()) == 1, f"len({threads}) != 1"
+
+
+def skip_on_gunicorn(func):
+    def wrapper(func):
+        if uwsgidecorators is None:
+            return
+        else:
+            return uwsgidecorators.postfork(func)
+
+    return wrapper(func)
+
+
+@skip_on_gunicorn
+def preload():
+    from trytond.transaction import Transaction
+    from trytond.cache import Cache
+    for db_name in db_list:
+        if db_name not in Cache._local.listeners:
+            if not Cache._clean_last:
+                Cache._clean_last = datetime.date.min
+            with Transaction().start(db_name, 0, readonly=True):
+                # Starting a transaction will trigger `Cache.sync`, which
+                # should spawn a thread to listen for cache invalidation and
+                # pool refresh events
+                pass
+        if db_name not in Cache._local.listeners:
+            raise AssertionError
