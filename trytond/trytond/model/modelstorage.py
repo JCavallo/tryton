23bf8740f54b1deb4949b114a82a101dbf4db331
Jean Cavallo <jean.cavallo@hotmail.fr>
2026-02-09 13:33:37 +0100

    PATCH P4: Export data access control

--- trytond/trytond/model/modelstorage.py
+++ trytond/trytond/model/modelstorage.py
@@ -872,6 +872,11 @@ class ModelStorage(Model):
 
     def __export_row(self, fields_names):
         pool = Pool()
+        ModelAccess = pool.get('ir.model.access')
+        ModelFieldAccess = pool.get('ir.model.field.access')
+        Rule = pool.get('ir.rule')
+        IrModel = pool.get('ir.model')
+
         lines = []
         data = ['' for x in range(len(fields_names))]
         done = []
@@ -893,6 +898,24 @@ class ModelStorage(Model):
                     field_name, language = field_name.split(':lang=')
                 eModel = pool.get(value.__name__)
                 field = eModel._fields[field_name]
+
+                ModelAccess.check(eModel.__name__, 'read')
+                ModelFieldAccess.check(eModel.__name__, [field_name], 'read')
+                domain = Rule.domain_get(eModel.__name__, mode='read')
+                if (domain
+                        and not eval_domain(
+                            domain, EvalEnvironment(value, eModel))):
+                    clause, clause_global = Rule.get(
+                        eModel.__name__, mode='read')
+                    rules = []
+                    rules.extend(clause.keys())
+                    rules.extend(clause_global.keys())
+                    raise AccessError(gettext(
+                            'ir.msg_read_rule_error',
+                            ids=str(value.id),
+                            model=IrModel.get_name(eModel.__name__),
+                            rules='\n'.join(r.name for r in rules)))
+
                 if field.states and 'invisible' in field.states:
                     invisible = _record_eval_pyson(
                         value, field.states['invisible'])
@@ -904,7 +927,10 @@ class ModelStorage(Model):
                 elif language:
                     value = field.translated().__get__(value, eModel)(language)
                 else:
-                    value = getattr(value, field_name)
+                    try:
+                        value = getattr(value, field_name)
+                    except AccessError:
+                        value = 'UNAUTHORIZED_401'
                 if field._type in {'one2many', 'many2many'}:
                     first = True
                     child_fields_names = [(x[:i + 1] == fields_tree[:i + 1]
