30c07936d47e8e481e08a24a4b333fa5aeced72a
Jean Cavallo <jean.cavallo@hotmail.fr>
2026-02-23 20:57:51 +0100

    [sao][Main] always_expand trees [CUSTOM]
    Add always_expand attribute for tree views inside O2M/M2M widgets. When
    set, child rows are automatically expanded on display and the expand
    toggle is handled consistently. Removes an unnecessary whitespace
    change in window.js.
    
    Files: view/form.js, view/tree.js, window.js
    Extraction ref: SA5-C / SA7-C (tree expansion)
    Source commits: part of 59172a9 (SA5 split), related to n_children/is_leaf

--- sao/src/view/form.js
+++ sao/src/view/form.js
@@ -6026,6 +6026,7 @@ function eval_pyson(value){
             this.el = jQuery('<div/>', {
                 'class': this.class_ + ' panel panel-default'
             });
+
             var heading = jQuery('<div/>', {
                 'class': this.class_ + '-heading panel-heading'
             }).appendTo(this.el);
